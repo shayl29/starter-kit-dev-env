5fb7ec7b330c82db5e56154ac775a3423ede1ba2
Shay Levinzon <shayl@wiropay.com>
2018-08-15 17:44:14 +0300

    adding mocha tests and Tranvis CI

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,6 @@
+// This file isn't transpiled so must use CommonJS or ES6
+
+// Register bable to transpile before our tests run
+require('babel-register')();
+
+require.extensions['.css'] = function () {};
